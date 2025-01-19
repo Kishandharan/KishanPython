3f8cd28d35dc05431527cedef22e531cd8cc2ae3
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-01-19 17:12:35 +0530

    Added matrix calculation - TODO: ADD INTERACTIVITY

--- 8.py
+++ 8.py
@@ -1,2 +1,18 @@
-# I'll do this later!
-# Goal: number fight game!
+from random import randint
+######################################
+game_matrix = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+]
+
+for x in game_matrix:
+    for y in range(0, len(x)):
+        x[y] = randint(0, 100)
+
+for z in game_matrix:
+    print(z)
+
+user_move = input("MV > ") # To be continued!
