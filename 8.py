f5becd6b67ecf350e15a0546b080f0662d56815c
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-01-20 15:21:47 +0530

    COMPLETED! AND IS ALSO WORKING WELL!

--- 8.py
+++ 8.py
@@ -1,8 +1,14 @@
 from random import randint
+from time import sleep
 import sys
 ######################################
 computer_number = 0
+used_moves = []
 rounds = int(input("How many rounds do you want to play? "))
+if rounds > 45:
+    print("This prompt cannot be over 45!")
+    sleep(5)
+    sys.exit()
 user_add_confirm = ""
 user_confirm_all = False
 user_move = ""
@@ -27,26 +33,38 @@ for z in game_matrix:
 
 for g in range(0, rounds):
     user_move = input("MV > ") # <row>-<row-item>, Example: 1-1
-    row = int(user_move.split("-")[0])-1
-    item = int(user_move.split("-")[1])-1
-    if not user_confirm_all:
-        user_add_confirm = input(f"Move to {game_matrix[row][item]} ([Y]es, [N]o, [A]ll)?").lower()
-        if user_add_confirm == "y":
-            pass
-        elif user_add_confirm == "n":
-            print(user_number)
-            continue
-        elif user_add_confirm == "a":
-            user_confirm_all = True
+    if user_move not in ["exit", "disp_matrix"]:
+        row = int(user_move.split("-")[0])-1
+        item = int(user_move.split("-")[1])-1
+        if not user_confirm_all:
+            user_add_confirm = input(f"Move to {game_matrix[row][item]} ([Y]es, [N]o, [A]ll)?").lower()
+            if user_add_confirm == "y":
+                pass
+            elif user_add_confirm == "n":
+                print(user_number)
+                continue
+            elif user_add_confirm == "a":
+                user_confirm_all = True
+            else:
+                print("Oops! It looks like you have entered something invalid! So we are defaulting to NO!")
+                continue
+        if not (user_move in used_moves):
+            if user_number >= game_matrix[row][item]:
+                user_number = user_number + game_matrix[row][item]
+                used_moves.append(user_move)
+            else:
+                print("Oops! You lost! Because the number you chose was more powerful than you!")
+                sys.exit()
         else:
-            print("Oops! It looks like you have entered something invalid! So we are defaulting to NO!")
-            continue
-    if user_number >= game_matrix[row][item]:
-        user_number = user_number + game_matrix[row][item]
+            print("Oops! It looks like you are trying to move to a point which you already used! Try again!")
     else:
-        print("Oops! You lost! Because the number you chose was more powerful than you!")
-        sys.exit()
-    if g != rounds:
+        if user_move == "disp_matrix":
+            for z in game_matrix:
+                print(z)
+        if user_move == "exit":
+            sys.exit()
+
+    if g != (rounds - 1):
         print(f"Your current score: {user_number}")
 
 print()
