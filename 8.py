cab11ffd22d27f71ffae65ddf9e24d7d7749becd
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-01-20 12:39:53 +0530

    Completed base functionality! TODO: Add additional commands, Prevent user from moving to same number multiple times

--- 8.py
+++ 8.py
@@ -1,12 +1,14 @@
 from random import randint
+import sys
 ######################################
 computer_number = 0
+rounds = int(input("How many rounds do you want to play? "))
 user_add_confirm = ""
 user_confirm_all = False
 user_move = ""
 row = 0
 item = 0
-user_number = 0
+user_number = 100
 game_matrix = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -14,16 +16,16 @@ game_matrix = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0]
 ]
-
-
+print("We gave you 100 points to help you get started!")
+print()
 for x in game_matrix:
     for y in range(0, len(x)):
-        x[y] = randint(0, 100)
+        x[y] = randint(0, 800)
 
 for z in game_matrix:
     print(z)
 
-for g in range(0, 10):
+for g in range(0, rounds):
     user_move = input("MV > ") # <row>-<row-item>, Example: 1-1
     row = int(user_move.split("-")[0])-1
     item = int(user_move.split("-")[1])-1
@@ -39,11 +41,19 @@ for g in range(0, 10):
         else:
             print("Oops! It looks like you have entered something invalid! So we are defaulting to NO!")
             continue
-    user_number = user_number + game_matrix[row][item]
-    print(user_number)
+    if user_number >= game_matrix[row][item]:
+        user_number = user_number + game_matrix[row][item]
+    else:
+        print("Oops! You lost! Because the number you chose was more powerful than you!")
+        sys.exit()
+    if g != rounds:
+        print(f"Your current score: {user_number}")
 
-computer_number = randint(1, 400)
+print()
+computer_number = randint(0, 800)
 if computer_number > user_number:
-    print("The computer won!")
+    print(f"Oops! You lost! Because the computer became more powerful than YOU!")
 else:
-    print("YOU won!")
+    print("Yay! YOU won!")
+print(f"Computer: {computer_number}")
+print(f"You: {user_number}")
