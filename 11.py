9b620c460b12fdbc40d711e062e2198c78e5c324
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-01-28 18:16:00 +0530

    Maze Game - To be continued...

---
+++ 11.py
@@ -0,0 +1,11 @@
+maze=[
+    ["#", "#", "#", "#", "#"],
+    ["#", "#", "#", "#", "#"],
+    ["#", "#", "#", "#", "#"],
+    ["#", "#", "#", "#", "#"],
+    ["#", "#", "#", "#", "#"],
+    ["#", "#", "#", "#", "#"]
+]
+
+# TODO: Make empty spaces in the mazes for the user to move,
+#       without all items getting converted to spaces.
