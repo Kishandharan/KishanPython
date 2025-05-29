525c7d514d80774e1ad1339113a09bbd64a75d6f
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-05-29 13:33:39 +0530

    Backup function working

--- 21.py
+++ 21.py
@@ -14,7 +14,8 @@ else:
     if args[1] == "backup":
         if os.path.isfile(args[2]):
             os.chdir(".ftt")
-            os.mkdir(args[2])
+            if not os.file.isdir(args[2]):
+                os.mkdir(args[2])
             os.chdir(args[2])
             if not os.path.isfile(".lstct"):
                 lstct_file=open(".lstct", "w")
@@ -23,7 +24,9 @@ else:
             else:
                 lstct_file=open(".lstct", "r")
                 lstct=lstct_file.read()
-
-
             file=open(args[2]+lstct, "w")
             shutil.copyfile(f"../../{args[2]}", f"./{args[2]+lstct}")
+            os.chdir("../")
+    if args[1] == "restore":
+        # To Be Continued
+
