2921c25de27de74726dd587a2ec6db658e6a02bf
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-01-09 04:41:24 +0400

    Fibonacci Sequence

---
+++ 63.py
@@ -0,0 +1,26 @@
+list1 = [0, 1]
+num1 = 0
+num2 = 1
+sum1 = num1+num2
+list1.append(sum1)
+print(list1)
+
+num1 = num2
+num2 = sum1
+sum1 = num1+num2
+list1.append(sum1)
+print(list1)
+
+num1 = num2
+num2 = sum1
+sum1 = num1+num2
+list1.append(sum1)
+print(list1)
+
+num1 = num2
+num2 = sum1
+sum1 = num1+num2
+list1.append(sum1)
+print(list1)
+
+
